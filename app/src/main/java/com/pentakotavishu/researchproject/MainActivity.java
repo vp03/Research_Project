dd8b97a865b6047c805a8849512b319a8a97b30a
vishpentak <vishpentak@gmail.com>
2020-09-29 13:34:39 -0400

    started authentication

--- app/src/main/java/com/pentakotavishu/researchproject/MainActivity.java
+++ app/src/main/java/com/pentakotavishu/researchproject/MainActivity.java
@@ -1,14 +1,39 @@
 package com.pentakotavishu.researchproject;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button registerBtn, loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeViews();
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Sign_Up.class);
+                startActivity(intent);
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void initializeViews() {
+        registerBtn = findViewById(R.id.register);
+        loginBtn = findViewById(R.id.login);
     }
 }
