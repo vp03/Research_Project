baad39dd96401e5901fa3b860a74bb4711e7d8c9
vishpentak <vishpentak@gmail.com>
2021-01-12 14:13:30 -0500

    Check to see if voice assistant is working

--- app/src/main/java/com/pentakotavishu/researchproject/Main_Activity.java
+++ app/src/main/java/com/pentakotavishu/researchproject/Main_Activity.java
@@ -1,115 +1,143 @@
 package com.pentakotavishu.researchproject;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import android.os.Bundle;
-
+import android.Manifest;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
 import java.io.IOException;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
+
 public class Main_Activity extends AppCompatActivity {
-    private Button startbtn, stopbtn, playbtn, stopplay;
+    private Button relocate;
+    private TextView textView;
     private MediaRecorder mRecorder;
     private MediaPlayer mPlayer;
     private static final String LOG_TAG = "AudioRecording";
     private static String mFileName = null;
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+    private StorageReference mStorageRef;
+    AppCompatActivity act;
+
+    private SpeechRecognizer speechRecognizer;
+    private TextToSpeech textToSpeech;
+    private Intent intent;
+
+    //hello
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startbtn = (Button)findViewById(R.id.btnRecord);
-        stopbtn = (Button)findViewById(R.id.btnStop);
-        playbtn = (Button)findViewById(R.id.btnPlay);
-        stopplay = (Button)findViewById(R.id.btnStopPlay);
-        stopbtn.setEnabled(false);
-        playbtn.setEnabled(false);
-        stopplay.setEnabled(false);
-        mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-        mFileName += "/AudioRecording.3gp";
 
-        startbtn.setOnClickListener(new View.OnClickListener() {
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        act = this;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
+        ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        textView = findViewById(R.id.textView);
+        relocate = findViewById(R.id.relocate);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
-            public void onClick(View v) {
-                if(CheckPermissions()) {
-                    stopbtn.setEnabled(true);
-                    startbtn.setEnabled(false);
-                    playbtn.setEnabled(false);
-                    stopplay.setEnabled(false);
-                    mRecorder = new MediaRecorder();
-                    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-                    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-                    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-                    mRecorder.setOutputFile(mFileName);
-                    try {
-                        mRecorder.prepare();
-                    } catch (IOException e) {
-                        Log.e(LOG_TAG, "prepare() failed");
+            public void onInit(int status) {
+            }
+
+        });
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+            }
+            @Override
+            public void onBeginningOfSpeech() {
+            }
+            @Override
+            public void onRmsChanged(float rmsdB) {
+            }
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+            }
+            @Override
+            public void onEndOfSpeech() {
+            }
+            @Override
+            public void onError(int error) {
+            }
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matches = results.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+                String string = "";
+                textView.setText("");
+                if (matches != null){
+                    string = matches.get(0);
+                    textView.setText(string);
+
+                    if (string.equals("start recording")){
+                        Intent intent = new Intent(Main_Activity.this, My_Post.class);
+                        startActivity(intent);
                     }
-                    mRecorder.start();
-                    Toast.makeText(getApplicationContext(), "Recording Started", Toast.LENGTH_LONG).show();
-                }
-                else
-                {
-                    RequestPermissions();
                 }
             }
-        });
-        stopbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(true);
-                mRecorder.stop();
-                mRecorder.release();
-                mRecorder = null;
-                Toast.makeText(getApplicationContext(), "Recording Stopped", Toast.LENGTH_LONG).show();
+            public void onPartialResults(Bundle partialResults) {
             }
-        });
-        playbtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(true);
-                mPlayer = new MediaPlayer();
-                try {
-                    mPlayer.setDataSource(mFileName);
-                    mPlayer.prepare();
-                    mPlayer.start();
-                    Toast.makeText(getApplicationContext(), "Recording Started Playing", Toast.LENGTH_LONG).show();
-                } catch (IOException e) {
-                    Log.e(LOG_TAG, "prepare() failed");
-                }
-            }
-        });
-        stopplay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mPlayer.release();
-                mPlayer = null;
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(false);
-                Toast.makeText(getApplicationContext(),"Playing Audio Stopped", Toast.LENGTH_SHORT).show();
+            public void onEvent(int eventType, Bundle params) {
             }
         });
     }
+
+    public void make_recording (){
+        Intent intent = new Intent(Main_Activity.this, My_Post.class);
+        startActivity(intent);
+    }
+
+    /*
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         switch (requestCode) {
@@ -126,12 +154,6 @@ public class Main_Activity extends AppCompatActivity {
                 break;
         }
     }
-    public boolean CheckPermissions() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
-        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
-    }
-    private void RequestPermissions() {
-        ActivityCompat.requestPermissions(Main_Activity.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
-    }
+     */
+
 }
