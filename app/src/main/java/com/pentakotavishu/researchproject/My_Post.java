1e49746bc3ffbbf118ba2e5a777bb34da9b9f2f2
Navya Vargese <navyavargese@Navyas-MBP.fios-router.home>
2021-01-20 13:02:25 -0500

    fix all the things that deleted

--- app/src/main/java/com/pentakotavishu/researchproject/My_Post.java
+++ app/src/main/java/com/pentakotavishu/researchproject/My_Post.java
@@ -1,32 +1,53 @@
 package com.pentakotavishu.researchproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.net.Uri;
-import android.os.Bundle;
-
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.io.IOException;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 
 public class My_Post extends AppCompatActivity {
     private Button startbtn, stopbtn, playbtn, stopplay, upload;
@@ -36,30 +57,39 @@ public class My_Post extends AppCompatActivity {
     private static String mFileName = null;
     public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
     private StorageReference mStorageRef;
+    AppCompatActivity act;
+
+    private SpeechRecognizer speechRecognizer;
+    private TextToSpeech textToSpeech;
+    private TextView textView;
+    private Intent intent;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        startbtn = (Button)findViewById(R.id.btnRecord);
-        stopbtn = (Button)findViewById(R.id.btnStop);
-        playbtn = (Button)findViewById(R.id.btnPlay);
-        stopplay = (Button)findViewById(R.id.btnStopPlay);
-        upload = (Button)findViewById(R.id.btnUpload);
-
-        stopbtn.setEnabled(false);
-        playbtn.setEnabled(false);
-        stopplay.setEnabled(false);
+        startbtn = findViewById(R.id.btnRecord);
+        stopbtn = findViewById(R.id.btnStop);
+        playbtn = findViewById(R.id.btnPlay);
+        stopplay = findViewById(R.id.btnStopPlay);
+        upload = findViewById(R.id.btnUpload);
         mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
         mFileName += "/AudioRecording.3gp";
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        act = this;
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+
 
         startbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(CheckPermissions()) {
-                    stopbtn.setEnabled(true);
-                    startbtn.setEnabled(false);
-                    playbtn.setEnabled(false);
-                    stopplay.setEnabled(false);
+                    startbtn.setVisibility(View.INVISIBLE);
+                    stopbtn.setVisibility(View.VISIBLE);
+                    stopplay.setVisibility(View.INVISIBLE);
+                    playbtn.setVisibility(View.INVISIBLE);
+                    upload.setVisibility(View.INVISIBLE);
                     mRecorder = new MediaRecorder();
                     mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
                     mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
@@ -82,10 +112,10 @@ public class My_Post extends AppCompatActivity {
         stopbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(true);
+                stopbtn.setVisibility(View.INVISIBLE);
+                startbtn.setVisibility(View.VISIBLE);
+                playbtn.setVisibility(View.VISIBLE);
+                upload.setVisibility(View.VISIBLE);
                 mRecorder.stop();
                 mRecorder.release();
                 mRecorder = null;
@@ -95,10 +125,10 @@ public class My_Post extends AppCompatActivity {
         playbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(false);
-                stopplay.setEnabled(true);
+                stopplay.setVisibility(View.VISIBLE);
+                playbtn.setVisibility(View.VISIBLE);
+                upload.setVisibility(View.VISIBLE);
+                startbtn.setVisibility(View.VISIBLE);
                 mPlayer = new MediaPlayer();
                 try {
                     mPlayer.setDataSource(mFileName);
@@ -115,10 +145,11 @@ public class My_Post extends AppCompatActivity {
             public void onClick(View v) {
                 mPlayer.release();
                 mPlayer = null;
-                stopbtn.setEnabled(false);
-                startbtn.setEnabled(true);
-                playbtn.setEnabled(true);
-                stopplay.setEnabled(false);
+                stopbtn.setVisibility(View.INVISIBLE);
+                startbtn.setVisibility(View.VISIBLE);
+                playbtn.setVisibility(View.VISIBLE);
+                stopplay.setVisibility(View.INVISIBLE);
+                upload.setVisibility(View.VISIBLE);
                 Toast.makeText(getApplicationContext(),"Playing Audio Stopped", Toast.LENGTH_SHORT).show();
             }
         });
@@ -126,62 +157,69 @@ public class My_Post extends AppCompatActivity {
         upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED ||
-                        ActivityCompat.checkSelfPermission(getApplication(), Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-                    // this will request for permission when permission is not true
-                    ActivityCompat.requestPermissions(getParent(), new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-                }
-                //Uri file = Uri.fromFile(new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)+"/mynotes.txt"));
-                Uri file = Uri.fromFile(new File(mFileName));
-                StorageReference notesRef = mStorageRef.child("audio/test.3gp");//could name the files based on time stamp
-                System.out.println(file.toString());
-                notesRef.putFile(file)
-                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                            @Override
-                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                                // Get a URL to the uploaded content
-                                Uri downloadUrl = taskSnapshot.getUploadSessionUri();
-                                System.out.println("testing: "+ downloadUrl);
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception exception) {
-                                // Handle unsuccessful uploads
-                                System.out.println("testing: "+ exception);
-                            }
-                        });
-                Toast.makeText(getApplicationContext(),"Uploaded recording", Toast.LENGTH_SHORT).show();
+                //upload to Firebase
+                upload();
+                Toast.makeText(getApplicationContext(),"Audio has been uploaded", Toast.LENGTH_SHORT).show();
+
             }
         });
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_AUDIO_PERMISSION_CODE:
-                if (grantResults.length> 0) {
-                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
-                    boolean permissionToStore = grantResults[1] ==  PackageManager.PERMISSION_GRANTED;
-                    if (permissionToRecord && permissionToStore) {
-                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(getApplicationContext(),"Permission Denied",Toast.LENGTH_LONG).show();
+
+        ActivityCompat.requestPermissions(this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        textView = findViewById(R.id.textView);
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+            }
+
+        });
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+/*
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle params) {
+            }
+            @Override
+            public void onBeginningOfSpeech() {
+            }
+            @Override
+            public void onRmsChanged(float rmsdB) {
+            }
+            @Override
+            public void onBufferReceived(byte[] buffer) {
+            }
+            @Override
+            public void onEndOfSpeech() {
+            }
+            @Override
+            public void onError(int error) {
+            }
+            @Override
+            public void onResults(Bundle results) {
+                ArrayList<String> matches = results.getStringArrayList(speechRecognizer.RESULTS_RECOGNITION);
+                String string = "";
+                textView.setText("");
+                if (matches != null){
+                    string = matches.get(0);
+                    textView.setText(string);
+                    if (string.equals("start recording")){
+                        startbtn();//make a method for button and link it to an action listener
                     }
                 }
-                break;
-        }
+            }
+            @Override
+            public void onPartialResults(Bundle partialResults) {
+            }
+            @Override
+            public void onEvent(int eventType, Bundle params) {
+            }
+        });
+ */
     }
-    public boolean CheckPermissions() {
-        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
-        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
-        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
-    }
-    private void RequestPermissions() {
-        ActivityCompat.requestPermissions(My_Post.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
-    }
-}
-/*
-   public void upload()
+
+
+    public void upload()
     {
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED ||
                 ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
@@ -209,4 +247,29 @@ public class My_Post extends AppCompatActivity {
                     }
                 });
     }
- */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length> 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] ==  PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),"Permission Denied",Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+    public boolean CheckPermissions() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+    private void RequestPermissions() {
+        ActivityCompat.requestPermissions(My_Post.this, new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+}
